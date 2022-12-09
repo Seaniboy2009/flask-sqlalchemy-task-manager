3d707d3f55e438cbe5d22eef6885c039dfcbfdf8
Sean Clark <seaniboy2009@yahoo.co.uk>
2022-12-09 13:07:13 +0000

    add task add

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,4 +3,16 @@ console.log("Connected")
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "select"}
+    });
+
+    // selector initialization
+    let selector = document.querySelectorAll('select');
+    M.FormSelect.init(selector);
+
   });
